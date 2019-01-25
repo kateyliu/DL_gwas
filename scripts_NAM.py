b4de314c4b6701c6290ecba9df90e7433ecb2849
alienware <yanglou1990@gmail.com>
2019-01-25 16:53:09 -0600

    add model

---
+++ scripts_NAM.py
@@ -0,0 +1,216 @@
+from __future__ import print_function
+import numpy as np
+import random
+import pandas as pd
+from scipy import stats
+import sys
+import logging
+import tensorflow as tf
+from keras import layers
+from keras import regularizers
+from keras.models import Model
+from keras.models import Sequential
+from keras.layers import *
+from keras.regularizers import l1,l2, L1L2
+from sklearn.metrics.pairwise import cosine_similarity
+import keras
+import keras.utils.np_utils as kutils
+from keras.optimizers import SGD
+from keras.callbacks import EarlyStopping,Callback,ModelCheckpoint,ReduceLROnPlateau
+from scipy.stats.stats import pearsonr
+from sklearn.metrics import mean_squared_error,r2_score,mean_absolute_error
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn import datasets, linear_model
+import itertools
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import math as m
+import keras.backend as K
+import sklearn 
+
+
+##one of K encoding
+
+nb_classes = 4
+def indices_to_one_hot(data, nb_classes):
+	
+	targets = np.array(data).reshape(-1)
+	
+	return np.eye(nb_classes)[targets]
+	
+
+	
+
+def readData(input):
+	data = pd.read_csv(input,sep='\t',header=None,na_values='nan')
+	SNP = data.iloc[:,2:].convert_objects(convert_numeric=True).values
+	YLD = data.iloc[:,1].convert_objects(convert_numeric=True).values
+	
+	arr = np.empty(shape=(SNP.shape[0],SNP.shape[1] , nb_classes))
+	for i in range(0,SNP.shape[0]):
+		arr[i] = indices_to_one_hot(pd.to_numeric(SNP[i],downcast='signed'), nb_classes)
+		
+	return arr,YLD
+
+'''	
+
+def multi_CNN(input):
+	
+	inputs = Input(shape=(input.shape[1],nb_classes))
+	
+	
+	x1 = Conv1D(10,4,padding='same',activation = 'linear', kernel_initializer = 'TruncatedNormal',kernel_regularizer=regularizers.l2(0.1),bias_regularizer = regularizers.l2(0.01))(inputs)
+	
+	x2 = Conv1D(10,20,padding='same',activation = 'linear', kernel_initializer = 'TruncatedNormal',kernel_regularizer=regularizers.l2(0.1),bias_regularizer = regularizers.l2(0.01))(inputs)
+	
+	x = concatenate([x1,x2]) 
+	
+	x = Dropout(0.75)(x)
+	
+	x = Conv1D(10,20,padding='same',activation = 'linear', kernel_initializer = 'TruncatedNormal',kernel_regularizer=regularizers.l2(0.01),bias_regularizer = regularizers.l2(0.01))(x) 
+	
+	x = Flatten()(x)
+	
+	x = Dropout(0.75)(x)
+	
+	outputs = Dense(1,activation = isru,kernel_initializer = 'glorot_normal',bias_regularizer = regularizers.l2(0.01))(x)
+	
+	model = Model(inputs = inputs,outputs = outputs)
+	model.compile(loss='mean_squared_error',optimizer=keras.optimizers.Adam(lr=0.001),metrics=['mae'])
+	
+	return model
+	
+'''
+	
+def resnet(input):
+	
+	inputs = Input(shape=(input.shape[1],nb_classes))
+	
+	
+	x = Conv1D(10,4,padding='same',activation = 'linear',kernel_initializer = 'TruncatedNormal', kernel_regularizer=regularizers.l2(0.1),bias_regularizer = regularizers.l2(0.01))(inputs)
+	
+	x = Conv1D(10,20,padding='same',activation = 'linear', kernel_initializer = 'TruncatedNormal',kernel_regularizer=regularizers.l2(0.1),bias_regularizer = regularizers.l2(0.01))(x)
+		
+	x = Dropout(0.75)(x)
+	
+	shortcut = Conv1D(10,4,padding='same',activation = 'linear',kernel_initializer = 'TruncatedNormal', kernel_regularizer=regularizers.l2(0.1),bias_regularizer = regularizers.l2(0.01))(inputs)
+	x = layers.add([shortcut,x])
+	
+	x = Conv1D(10,4,padding='same',activation = 'linear',kernel_initializer = 'TruncatedNormal', kernel_regularizer=regularizers.l2(0.1),bias_regularizer = regularizers.l2(0.01))(x)
+	x = Dropout(0.75)(x)
+	x = Flatten()(x)
+	
+	x = Dropout(0.75)(x)
+	
+	outputs = Dense(1,activation = isru,bias_regularizer = regularizers.l2(0.01),kernel_initializer = 'TruncatedNormal',name = 'out')(x)
+	
+	model = Model(inputs = inputs,outputs = outputs)
+	model.compile(loss='mean_squared_error',optimizer=keras.optimizers.Adam(lr=0.001),metrics=['mae'])
+	
+	return model
+
+def compile_saliency_function(model):
+	inp = model.layers[0].input
+	outp = model.layers[10].output
+	max_outp = K.max(outp, axis=1)
+	saliency = K.gradients(K.sum(max_outp), inp)
+	return K.function([inp,K.learning_phase()], saliency)
+	
+	
+	
+def show_images_plot(saliency,wald,outname):
+	plt.figure(figsize=(15, 8), facecolor='w')
+	
+	plt.subplot(2, 1, 1)
+	x = np.median(saliency,axis=-1)
+	plt.plot(x,'b.')
+	line = sorted(x,reverse = True)[10]
+	plt.axhline(y = line,color='b', linestyle='--')
+	plt.ylabel('saliency value', fontdict=None, labelpad=None,fontsize=15)
+	
+	
+	plt.subplot(2, 1, 2)
+	plt.plot(wald,'r1')
+	line = sorted(wald,reverse = True)[10]
+	plt.axhline(y = line,color='r', linestyle='--')
+	
+	plt.xlabel('SNPs', fontdict=None, labelpad=None,fontsize=15)
+	plt.ylabel('Wald', fontdict=None, labelpad=None,fontsize=15)
+	
+	plt.savefig(outname)
+	plt.clf()
+	plt.cla()
+	plt.close()
+
+	
+	
+def get_saliency(testSNP,model):
+	
+	array= np.array([testSNP])
+	saliency_fn = compile_saliency_function(model)
+	saliency_out = saliency_fn([[y for y in array][0],1])
+	saliency = saliency_out[0]
+	saliency = saliency[::-1].transpose(1, 0, 2)
+	output= np.abs(saliency).max(axis=-1)
+	
+	return output
+
+# a= 0.5
+def isru(x):
+	return  x/(K.sqrt(1+0.5*K.square(x)))
+	
+	
+def main():
+	
+	polyp = pd.read_csv("polytest.txt",sep='\t',header=0,na_values='nan')
+	wald =  polyp.iloc[:,0].convert_objects(convert_numeric=True).values
+	
+	test_corr=[]
+	
+	batch_size=250
+	earlystop = 5
+	early_stopping = EarlyStopping(monitor='val_mean_absolute_error', patience=earlystop)
+	
+	for i in range(1,11):
+	
+		trainSNP, trainYLD  = readData("SoyNAM/imp/4236_imp_train_"+str(i)+".txt")
+		testSNP, testYLD = readData("SoyNAM/imp/4236_imp_test_"+str(i)+".txt")	
+		valSNP, valYLD = readData("SoyNAM/imp/4236_imp_val_"+str(i)+".txt")	
+		
+		model = resnet(trainSNP)
+		
+		history  = model.fit(trainSNP, trainYLD, batch_size=batch_size, epochs=1000,validation_data=(valSNP,valYLD),callbacks=[early_stopping],shuffle= True)
+		preytest = model.predict(testSNP)
+		preytest.shape = (preytest.shape[0],)
+		
+		corr = pearsonr(preytest,testYLD)[0]
+		test_corr.append(float('%0.4f' % corr))
+		
+		print ('Accuracy of folds'+ str(i) +'is :' + str(corr))
+		
+		tmp = get_saliency(testSNP,model)
+		
+		if i == 1:
+		
+			saliency = tmp
+			
+		else:
+			saliency = np.concatenate((saliency,tmp),axis=-1)
+		
+		
+	print ('Accuracy of 10 fold cross validation is '+ str(np.mean(test_corr)))
+	show_images_plot(saliency,wald,"saliency_out.png")
+	
+
+
+if __name__ == '__main__':
+   main()
+
+   
+
+
+	
+
